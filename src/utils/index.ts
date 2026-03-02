99c1260a23e1879e5e8907ab359081d660b08909
robert castro <rscg14@gmail.com>
2026-03-02 14:46:12 -0500

    feat: add utils enums (Images, QueryKeys, Routes)

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+export enum Images {
+  Logo = '/logo1.png',
+  CardBack = '/ricky_morty_img.png',
+}
+
+export enum QueryKeys {
+  Characters = 'characters',
+}
+
+export enum Routes {
+  Login = '/login',
+  Home = '/',
+  Game = '/game',
+  Result = '/result',
+}
